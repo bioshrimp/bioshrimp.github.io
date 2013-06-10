95772f4e63247f1dd02765ad6088e81ad375bee1
Reb <bexi@freenet.de>
2013-06-10 21:22:57 +0200

    removed

--- config.rb
+++ config.rb
@@ -4,10 +4,10 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/bioshrimp/"
-http_images_path = "/bioshrimp/images"
-http_fonts_path = "/bioshrimp/fonts"
-css_dir = "public/bioshrimp/stylesheets"
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "public/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
